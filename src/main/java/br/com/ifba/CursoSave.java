244460c9f67d4660125b38f46ef051166bba9043
Luan Alves de Souza <a15910a@gmail.com>
2025-06-13 13:44:14 -0300

    Atividade 12 prg03

---
+++ src/main/java/br/com/ifba/CursoSave.java
@@ -0,0 +1,13 @@
+package br.com.ifba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSave {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSave.class, args);
+	}
+
+}
