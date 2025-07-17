d121b172d398165843c0db1b1c4a1768dfa7513e
ZeroLuan <alvesluanpc@gmail.com>
2025-07-17 20:27:53 -0300

    atividade 17

--- Spring-Project/prg03persistenciaspring/src/main/java/br/com/ifba/curso/repository/CursoRepository.java
+++ Spring-Project/prg03persistenciaspring/src/main/java/br/com/ifba/curso/repository/CursoRepository.java
@@ -6,12 +6,14 @@ package br.com.ifba.curso.repository;
 
 import br.com.ifba.curso.entity.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author a1591
  */
 //Interface para poder persistir os dados no banco 
+@Repository 
 public interface CursoRepository extends JpaRepository<Curso, Long>{
     
 }
